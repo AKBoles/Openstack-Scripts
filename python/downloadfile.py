59184191105544a5dfb3549bab066f435648b188
Andrew Boles <drew.boles88@gmail.com>
2016-11-01 12:18:09 -0500

    Update downloadfile.py

--- python/downloadfile.py
+++ python/downloadfile.py
@@ -2,6 +2,21 @@
 from swiftclient.service import SwiftService, SwiftError
 from sys import argv
 
+# delete a list of objects in a container
+def delete_files(container, objs):
+	# first argument is the container where objects are located
+	# second argument is a list containing all of the objects to delete
+	with SwiftService() as swift:
+    		try:
+			delete_iter = swift.delete(container=container, objects=objs)
+        		for result in delete_iter:
+            			if result["success"]:
+					print('Object %s was successfully deleted.' %result.get('object',''))
+            			else:
+					print('Object %s was not successfully deleted.' %result.get('object',''))
+    		except SwiftError as e:
+			print('Error: %s' %e)
+
 # argument one is the container name
 # argument two is the folder name within the container
 # argument three is a flag to delete file from container if set true
